10aaa8e0b061a253def6cf85c47af4b3a5b95501
Irena <irenejpopova6@gmail.com>
2020-02-29 15:56:51 +0100

    add rout

--- catalog/db_setup.py
+++ catalog/db_setup.py
@@ -1,12 +1,33 @@
+# this file create the empty database
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
-class Categorie(Base):
-    __tablename__ = 'categorie'
+class Category(Base):
+    __tablename__ = 'category'
+    # we need a primary key for every Bases
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    
+
+# create a relation between book and category
+
+
+class Book(Base):
+    __tablename__ = 'book'
+    # we need a primary key for every Bases
+    id = Column(Integer, primary_key=True)
+    category = Column(Integer, ForeignKey('category.id'))
+    name = Column(String(250), nullable=False)
+    author = Column(String(250), nullable=False)
+
+
+engine = create_engine('sqlite:///db.db')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
--- catalog/main.py
+++ catalog/main.py
@@ -1,16 +1,36 @@
 from flask import Flask, render_template
+from db_setup import session
+from db_setup import Category, Book  # FETCH, define THE CATEGORY OBJECT
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the forum.'''
-  # TODO Fetch it from db
-  categories = ["History", "Geography", "Fiction", "Prose"] 
-  return render_template("index.html", categories=categories)
+    '''Main page of the forum.'''
+    # TODO Fetch it from db
+
+    # User is the name of table that has a column name
+    categories = session.query(Category).all()
+    books = session.query(Book).all()
+
+    return render_template("index.html", categories=categories, books=books)
+
+
+@app.route('/login')
+def login():
+    return "<div class='login'>Login</div>"
+
+
+@app.route('/books/<id>')
+def book_detail(id):
+    return render_template("book.html", id=id)
+# fetches the data
+
 
 app.config["TESTING"] = True
 app.config["FLASK_ENV"] = "development"
-app.config["TEMPLATES_AUTO_RELOAD"] = True 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 app.run(host='0.0.0.0', port=8000, debug=True)
+# main.py is running the server and
