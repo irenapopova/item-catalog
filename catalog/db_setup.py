1b9c0aa86b76c43ab75814a0fe35ff16ae3aad57
Irena <irenejpopova6@gmail.com>
2020-04-02 18:34:21 +0200

    database added

--- catalog/db_setup.py
+++ catalog/db_setup.py
@@ -1,6 +1,6 @@
 # this file create the empty database
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -24,6 +24,25 @@ class Book(Base):
     category = Column(Integer, ForeignKey('category.id'))
     name = Column(String(250), nullable=False)
     author = Column(String(250), nullable=False)
+    # the price column should have a value
+    price = Column(Float, nullable=False)
+    description = Column(String(500), nullable=False)
+    image = Column(String(300), nullable=False)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    firstName = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey("user.id"))
+    book = Column(Integer, ForeignKey("book.id"))
 
 
 engine = create_engine('sqlite:///db.db')
