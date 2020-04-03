9ef4b2cd1d7c814add813686a9eaae993d02b872
Irena <irenejpopova6@gmail.com>
2020-04-03 20:43:01 +0200

    api link

--- catalog/api.py
+++ catalog/api.py
@@ -12,24 +12,41 @@ def get_categories():
     return jsonify(data)
 
 
+# 1 - 1xx -101,102,120,199 - Information
+# 2 - 2xx - 200,201,       - Success
+# 3 - 3xx - 301,31         - Redirected
+# 4 - 4xx - 400            - client mistake
+# 5 - 5xx - 500            - server error
+
 @app.route('/books', methods=['GET'])
 def get_books():
     books = session.query(Book).all()
     data = [i.serialize for i in books]
+    # this generate the json file
     return jsonify(data)
 
 # get books by category id
 @app.route('/category/<int:id>', methods=['GET'])
 def get_category_by_id(id):
     category = session.query(Category).get(id)
+    # this generates json file
     return jsonify(category.serialize)
 
 
 @app.route('/books/<int:id>', methods=['GET'])
 def get_books_by_id(id):
     books = session.query(Book).get(id)
-
     return jsonify(books.serialize)
+
+
+@app.route('/category/<int:id>/books', methods=['GET'])
+def get_books_in_category(id):
+    books = session.query(Book).all()
+    # for -> loop all books
+    # filter book which has the same category id
+    # make them as json
+    data = [book.serialize for book in books if book.category == id]
+    return jsonify(data)
 
 
 if __name__ == "__main__":
